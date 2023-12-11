bc76af7bad215eb3b0bd5f0d56e3edc5d9a77556
정상우 <zabefofoon@gmail.com>
2023-12-11 21:54:56 +0900

    개인정보처리방침 추가

--- src/App.vue
+++ src/App.vue
@@ -1,85 +1,77 @@
-<script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+  Privacy Policy<br /><br />
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+  Who we are<br />
+  Our website address is: https://mm-embedded.netlify.app/<br /><br />
 
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
+  Comments<br />
+  When visitors leave comments on the site we collect the data shown in the
+  comments form, and also the visitor’s IP address and browser user agent string
+  to help spam detection.<br /><br />
 
-  <RouterView />
+  An anonymized string created from your email address (also called a hash) may
+  be provided to the Gravatar service to see if you are using it. The Gravatar
+  service privacy policy is available here: https://automattic.com/privacy/.
+  After approval of your comment, your profile picture is visible to the public
+  in the context of your comment.<br /><br />
+
+  Media<br />
+  If you upload images to the website, you should avoid uploading images with
+  embedded location data (EXIF GPS) included. Visitors to the website can
+  download and extract any location data from images on the website.<br /><br />
+
+  Cookies<br />
+  If you leave a comment on our site you may opt-in to saving your name, email
+  address and website in cookies. These are for your convenience so that you do
+  not have to fill in your details again when you leave another comment. These
+  cookies will last for one year.<br /><br />
+
+  If you visit our login page, we will set a temporary cookie to determine if
+  your browser accepts cookies. This cookie contains no personal data and is
+  discarded when you close your browser.<br /><br />
+
+  When you log in, we will also set up several cookies to save your login
+  information and your screen display choices. Login cookies last for two days,
+  and screen options cookies last for a year. If you select "Remember Me", your
+  login will persist for two weeks. If you log out of your account, the login
+  cookies will be removed.<br /><br />
+
+  If you edit or publish an article, an additional cookie will be saved in your
+  browser. This cookie includes no personal data and simply indicates the post
+  ID of the article you just edited. It expires after 1 day.<br /><br />
+
+  Embedded content from other websites<br />
+  Articles on this site may include embedded content (e.g. videos, images,
+  articles, etc.). Embedded content from other websites behaves in the exact
+  same way as if the visitor has visited the other website.<br /><br />
+
+  These websites may collect data about you, use cookies, embed additional
+  third-party tracking, and monitor your interaction with that embedded content,
+  including tracking your interaction with the embedded content if you have an
+  account and are logged in to that website.<br /><br />
+
+  Who we share your data with<br />
+  If you request a password reset, your IP address will be included in the reset
+  email.<br /><br />
+  ​ How long we retain your data<br />
+  If you leave a comment, the comment and its metadata are retained
+  indefinitely. This is so we can recognize and approve any follow-up comments
+  automatically instead of holding them in a moderation queue.<br /><br />
+
+  For users that register on our website (if any), we also store the personal
+  information they provide in their user profile. All users can see, edit, or
+  delete their personal information at any time (except they cannot change their
+  username). Website administrators can also see and edit that information.<br /><br />
+
+  What rights you have over your data<br />
+  If you have an account on this site, or have left comments, you can request to
+  receive an exported file of the personal data we hold about you, including any
+  data you have provided to us. You can also request that we erase any personal
+  data we hold about you. This does not include any data we are obliged to keep
+  for administrative, legal, or security purposes.<br /><br />
+
+  Where your data is sent<br />
+  Visitor comments may be checked through an automated spam detection service.
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
-}
-</style>
+<style scoped></style>
